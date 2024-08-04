2ce0c370992e4d98f7678d2f8d39e781b11228c5
unknown <cs50riyam211@gmail.com>
2024-08-04 20:51:37 +0300

    fruit hub app

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:fruit/features/splash/presentation/views/splash_view.dart';
+
+void main() {
+  runApp(const FruitHub());
+}
+
+class FruitHub extends StatelessWidget {
+  const FruitHub({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SplashView(),
+    );
+  }
+}
