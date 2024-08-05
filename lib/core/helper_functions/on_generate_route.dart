c2bb27d4a15b62ba8f3b95f3356cd95fa71e08e1
Riyam224 <cs50riyam221@gmail.com>
2024-08-06 00:09:42 +0300

    excute navigation in the splash view body to move to the on boarding view

--- lib/core/helper_functions/on_generate_route.dart
+++ lib/core/helper_functions/on_generate_route.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:fruit/features/on_boarding/presentation/views/on_boarding_view.dart';
 import 'package:fruit/features/splash/presentation/views/splash_view.dart';
 
 Route<dynamic> onGenerateRoute(RouteSettings settings) {
   switch (settings.name) {
     case SplashView.routeName:
       return MaterialPageRoute(builder: (context) => const SplashView());
+
+    case OnBoardingView.routeName:
+      return MaterialPageRoute(builder: (context) => const OnBoardingView());
+
     default:
       return MaterialPageRoute(builder: (context) => const Scaffold());
   }
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,27 +1,39 @@
-// ignore_for_file: depend_on_referenced_packages
-
 import 'package:flutter/material.dart';
+import "package:flutter_svg/flutter_svg.dart";
 import 'package:fruit/core/utils/app_images.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fruit/features/on_boarding/presentation/views/on_boarding_view.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
   Widget build(BuildContext context) {
-    return Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-      Row(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          SvgPicture.asset(Assets.assetsImagesPlant),
-        ],
-      ),
-      SvgPicture.asset(Assets.assetsImagesLogo),
-      SvgPicture.asset(
-        Assets.assetsImagesSplashBottom,
-        fit: BoxFit.fill,
-      ),
-    ]);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SvgPicture.asset(Assets.assetsImagesPlant),
+          ],
+        ),
+        SvgPicture.asset(Assets.assetsImagesLogo),
+        SvgPicture.asset(Assets.assetsImagesSplashBottom),
+      ],
+    );
+  }
+
+  // todo
+  void excuteNavigation() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(context,
+          MaterialPageRoute(builder: (context) => const OnBoardingView()));
+    });
   }
 }
